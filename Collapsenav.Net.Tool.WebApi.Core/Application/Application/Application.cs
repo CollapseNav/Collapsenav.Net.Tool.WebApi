59034f14457ac06f92c566f387d5126aa45ac55b
CollapseNav <CollapseNav@163.com>
2024-03-05 22:38:09 +0800

    fix: 适配data包的修改

--- Collapsenav.Net.Tool.WebApi.Core/Application/Application/Application.cs
+++ Collapsenav.Net.Tool.WebApi.Core/Application/Application/Application.cs
@@ -1,7 +1,7 @@
 using Collapsenav.Net.Tool.Data;
 namespace Collapsenav.Net.Tool.WebApi;
 
-public class Application<T> : IApplication<T> where T : IEntity
+public class Application<T> : IApplication<T> where T : class, IEntity
 {
     protected IRepository<T> Repo;
     public Application(IRepository<T> repository)
