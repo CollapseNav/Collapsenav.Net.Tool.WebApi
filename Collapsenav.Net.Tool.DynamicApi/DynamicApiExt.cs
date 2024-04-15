d289a619dd19f85d23007accda3ac1b135b8d40e
CollapseNav <CollapseNav@163.com>
2024-04-15 15:53:40 +0800

    pref

--- Collapsenav.Net.Tool.DynamicApi/DynamicApiExt.cs
+++ Collapsenav.Net.Tool.DynamicApi/DynamicApiExt.cs
@@ -54,26 +54,55 @@ public static class DynamicApiExt
         });
         return builder;
     }
-
+    /// <summary>
+    /// 注册controller
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     internal static DynamicController AddController<T>(this IServiceCollection services, string route)
     {
         var controller = new DynamicController(typeof(T), route);
         ApplicationServiceConvention.DynamicControllers.Add(controller);
         return controller;
     }
+    /// <summary>
+    /// 注册controller
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="controllerType"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     internal static DynamicController AddController(this IServiceCollection services, Type controllerType, string route)
     {
         var controller = new DynamicController(controllerType, route);
         ApplicationServiceConvention.DynamicControllers.Add(controller);
         return controller;
     }
+    /// <summary>
+    /// 注册用于查询的controller
+    /// </summary>
+    /// <typeparam name="Entity"></typeparam>
+    /// <typeparam name="GetDto"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     public static DynamicController AddQueryController<Entity, GetDto>(this IServiceCollection services, string route)
         where Entity : class, IEntity
         where GetDto : IBaseGet<Entity>
     {
         return services.AddController<QueryAppController<Entity, GetDto>>(route);
     }
-
+    /// <summary>
+    /// 注册Crud controller
+    /// </summary>
+    /// <typeparam name="Entity"></typeparam>
+    /// <typeparam name="CreateDto"></typeparam>
+    /// <typeparam name="GetDto"></typeparam>
+    /// <param name="services"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     public static DynamicController AddCrudController<Entity, CreateDto, GetDto>(this IServiceCollection services, string route)
         where Entity : class, IEntity
         where GetDto : IBaseGet<Entity>
@@ -81,45 +110,68 @@ public static class DynamicApiExt
     {
         return services.AddController<CrudAppController<Entity, CreateDto, GetDto>>(route);
     }
-
+    /// <summary>
+    ///  注册用于查询的controller
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="entityType"></param>
+    /// <param name="getDtoType"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     public static DynamicController AddQueryController(this IServiceCollection services, Type entityType, Type getDtoType, string route)
     {
         var controller = new DynamicController(entityType, getDtoType, route);
         ApplicationServiceConvention.DynamicControllers.Add(controller);
         return controller;
     }
+    /// <summary>
+    /// 注册Crud controller
+    /// </summary>
+    /// <param name="services"></param>
+    /// <param name="entityType"></param>
+    /// <param name="createDtoType"></param>
+    /// <param name="getDtoType"></param>
+    /// <param name="route"></param>
+    /// <returns></returns>
     public static DynamicController AddCrudController(this IServiceCollection services, Type entityType, Type createDtoType, Type getDtoType, string route)
     {
         var controller = new DynamicController(entityType, createDtoType, getDtoType, route);
         ApplicationServiceConvention.DynamicControllers.Add(controller);
         return controller;
     }
-
+    /// <summary>
+    /// 自动根据注解添加controller
+    /// </summary>
+    /// <param name="services"></param>
+    /// <returns></returns>
     public static IServiceCollection AddAutoController(this IServiceCollection services)
     {
-        // 先查出所有的继承IBaseGet和IBaseCreate的类型
-        var types = AppDomain.CurrentDomain.GetTypes(typeof(IBaseGet), typeof(IBaseCreate));
+        // 先查出所有的继承IBaseGet和IBaseCreate的类型, 作为泛型参数
+        var markedTypes = AppDomain.CurrentDomain.GetTypes(typeof(IBaseGet), typeof(IBaseCreate));
         // 然后根据MapControllerAttribute的Value分组（就是controller的路由）
-        var controllerGroups = types.Where(i => i.HasAttribute<MapControllerAttribute>()).GroupBy(i => i.GetCustomAttribute<MapControllerAttribute>()!.Value).ToList();
+        var controllerGroups = markedTypes.Where(i => i.HasAttribute<MapControllerAttribute>())
+                            .GroupBy(i => i.GetCustomAttribute<MapControllerAttribute>()!.Value).ToList();
         // 然后创建对应的controller
-        foreach (var controllerGroup in controllerGroups)
+        foreach (var (route, types) in controllerGroups.Select(i => (i.Key, i.ToList())))
         {
+            if (route.IsEmpty() || types.IsEmpty())
+                continue;
+            var createTypes = types.Where(i => i.IsType<IBaseCreate>()).ToList();
+            var getTypes = types.Where(i => i.IsType<IBaseGet>()).ToList();
             Type? entityType = null;
             Type? getType = null;
             Type? createType = null;
-            List<Type> getTypes = new();
             List<Type> getPageTypes = new();
             DynamicController? controller = null;
 
-            if (controllerGroup.Any(i => i.IsType(typeof(IBaseCreate))))
+            if (createTypes.Any(i => i.IsType(typeof(IBaseCreate))))
             {
-                createType = controllerGroup.First(i => i.IsType(typeof(IBaseCreate)));
+                createType = createTypes.First(i => i.IsType(typeof(IBaseCreate)));
             }
 
             // 找到继承IBaseGet和IBaseCreate的类型
-            if (controllerGroup.Any(i => i.IsType(typeof(IBaseGet))))
+            if (types.Any(i => i.IsType(typeof(IBaseGet))))
             {
-                getTypes = controllerGroup.Where(i => i.IsType(typeof(IBaseGet))).ToList();
                 if (getTypes.Count == 1 && !getTypes.First().IsType(typeof(IBaseJoinGet<,>)))
                 {
                     getType = getTypes.First();
@@ -146,7 +198,7 @@ public static class DynamicApiExt
             else if (createType == null)
             {
                 entityType = getType.BaseType!.GenericTypeArguments.First(i => i.IsType(typeof(IEntity)));
-                controller = services.AddQueryController(entityType, getType, controllerGroup.Key);
+                controller = services.AddQueryController(entityType, getType, route);
             }
             // 如果有create，则认为是增删改查controller
             else if (createType != null)
@@ -155,7 +207,7 @@ public static class DynamicApiExt
                 var createEntityType = createType.BaseType!.GenericTypeArguments.First(i => i.IsType(typeof(IEntity)));
                 // 此时需要保证get和create的实体类型一致
                 if (entityType == createEntityType)
-                    controller = services.AddCrudController(entityType, createType, getType, controllerGroup.Key);
+                    controller = services.AddCrudController(entityType, createType, getType, route);
                 else
                     continue;
             }
--- Demo/WebApiUseModule/Program.cs
+++ Demo/WebApiUseModule/Program.cs
@@ -1,40 +1,16 @@
 using Collapsenav.Net.Tool;
-using Collapsenav.Net.Tool.AutoInject;
 using Collapsenav.Net.Tool.Data;
 using Collapsenav.Net.Tool.DynamicApi;
 using Collapsenav.Net.Tool.WebApi;
 using Collapsenav.WebApi.Module;
 using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.LoadModules();
-// builder.Services.AddCrudController<FirstEntity, FirstCreateDto, FirstGetDto>("testdy")
-// .AddGetAction<FirstJoinGetDto>("QueryNew")
-// .AddGetAction<FirstJoinGetDto2>("QueryTTTT");
-builder.Services.AddSqlitePool<EntityContext>(new SqliteConn("./Data.db"));
-builder.Services.AddDefaultDbContext<EntityContext>();
-
 var app = builder.Build();
 app.UseAutoCommit();
 if (app.Environment.IsDevelopment()) app.UseSwagger().UseSwaggerUI();
 app.MapControllers();
 app.Run();
-
-[DynamicApi]
-public class TestApi
-{
-    [AutoInject]
-    public IRepository<FirstEntity>? FirstRepo { get; set; }
-
-    public string QueryBBBB()
-    {
-        Console.WriteLine(FirstRepo == null);
-        return "123";
-    }
-}
-
-
-#region dfoasdjf
 public class EntityContext : DbContext
 {
     public DbSet<FirstEntity> FirstEntity { get; set; }
@@ -58,17 +34,13 @@ public class SecondEntity : BaseEntity<long?>
     public int? Age { get; set; }
     public string? Description { get; set; }
 }
-
 [MapController("testdy")]
 public class FirstCreateDto : BaseCreate<FirstEntity>
 {
     public long? Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public override bool IsExist(IQueryable<FirstEntity>? rep)
-    {
-        return rep.WhereIf(Name, item => item.Name == Name).Any();
-    }
+    public override bool IsExist(IQueryable<FirstEntity>? rep) => rep.WhereIf(Name, item => item.Name == Name).Any();
 }
 [MapController("testdy")]
 public class FirstGetDto : BaseGet<FirstEntity>
@@ -76,41 +48,30 @@ public class FirstGetDto : BaseGet<FirstEntity>
     public long? Id { get; set; }
     public string? Name { get; set; }
     public string? Description { get; set; }
-    public override IQueryable<FirstEntity> GetQuery(IRepository<FirstEntity> query)
-    {
-        return query.Query()
-        .WhereIf(Id.HasValue, item => item.Id == Id)
+    public override IQueryable<FirstEntity> GetQuery(IRepository<FirstEntity> query) => query.Query().WhereIf(Id.HasValue, item => item.Id == Id)
         .WhereIf(Name, item => item.Name == Name)
         .WhereIf(Description, item => item.Description.Contains(Description))
         ;
-    }
 }
 [MapController("testdy", "QueryNew", false)]
 public class FirstJoinGetDto : BaseJoinGet<FirstEntity, ReturnModel>
 {
     public long? Id { get; set; }
     public string? Name { get; set; }
-    public override IQueryable<ReturnModel?> GetQuery(IRepository<FirstEntity> repo)
-    {
-        return repo
-        .LeftJoin<SecondEntity>(i => i.Name, i => i.Description)
+    public override IQueryable<ReturnModel?> GetQuery(IRepository<FirstEntity> repo) => repo.LeftJoin<SecondEntity>(i => i.Name, i => i.Description)
         .WhereIf(Id.HasValue, item => item.Data1.Id == Id)
-        .WhereIf(Name, item => item.Data1.Name == Name)
+        .WhereIf(Name, item => item.Data1.Name.Contains(Name))
         .SelectValue((f, s) => new ReturnModel
         {
             Id = f?.Id,
             Name = f?.Name,
             Age = s?.Age.ToString(),
         });
-    }
 }
 public class FirstJoinGetDto2 : BaseJoinGet<FirstEntity, ReturnModel>
 {
     public long? Id { get; set; }
-    public override IQueryable<ReturnModel> GetQuery(IRepository<FirstEntity> repo)
-    {
-        return repo
-        .LeftJoin<SecondEntity>(i => i.Name, i => i.Description)
+    public override IQueryable<ReturnModel> GetQuery(IRepository<FirstEntity> repo) => repo.LeftJoin<SecondEntity>(i => i.Name, i => i.Description)
         .WhereIf(Id.HasValue, item => item.Data1.Id == Id)
         .Select(item => new ReturnModel
         {
@@ -118,14 +79,10 @@ public class FirstJoinGetDto2 : BaseJoinGet<FirstEntity, ReturnModel>
             Name = item.Data1.Name,
             Age = item.Data2.Age.ToString(),
         });
-    }
 }
-
 public class ReturnModel
 {
     public long? Id { get; set; }
     public string? Name { get; set; }
     public string? Age { get; set; }
 }
-
-#endregion
